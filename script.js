0cb5086daaac13ef337559ee5c565b56aa3d50ca
GrantMWu <gmwu812@gmail.com>
2023-11-30 08:22:30 -0600

    Add change grid size button
    Add button that allows user to change the grid sizes by inputting number.

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 sketchContainer = document.querySelector("#sketch-container")
 
-createGrid(20)
+createGrid(16);
 
-let grids = document.querySelectorAll('.grid-block');
-
-grids.forEach(function(node) {
-    node.addEventListener('mouseover', () => node.style.backgroundColor = 'gray');
-});
+document.querySelector('#grid-change').addEventListener('click', changeGrid);
 
 function createGrid(num) {
     for (row = 0; row < num; row++) {
@@ -17,6 +13,24 @@ function createGrid(num) {
             const newDiv = document.createElement('div');
             newDiv.classList.add('grid-block');
             newRow.appendChild(newDiv);
+        }
+    }
+    let grids = document.querySelectorAll('.grid-block');
+    
+    grids.forEach(function(grid) {
+        grid.addEventListener('mousedown', () => grid.style.backgroundColor = 'gray')
+    });
+}
+
+function changeGrid () {
+    let num = 0;
+    while(true) {
+        num = prompt('Enter a number between 2 and 100 to change to a nxn grid');
+        if (num < 2 || num > 100) {
+          alert('Invalid entry');
+        } else {
+            createGrid(num)
+            break;
         }
     }
 }
